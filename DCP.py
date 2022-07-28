f43906d55aa28c7dcc9e3bcb46b7c1a6b75f2db6
kinan <kinanbezem7@gmail.com>
2022-07-28 13:33:24 +0100

    fixed bug in save data method

--- DCP.py
+++ DCP.py
@@ -81,14 +81,16 @@ class Scraper:
         """
         book_container =self. driver.find_element(by=By.XPATH, value='//div[@class="search-results-list"]') # XPath corresponding to the Container
         book_list = book_container.find_elements(by=By.XPATH, value='./div')
-        self.link_list = []
+        link_list = []
 
         for book_property in book_list:
             a_tag = book_property.find_element(by=By.TAG_NAME, value='a')
             link = a_tag.get_attribute('href')
-            self.link_list.append(link)
+            link_list.append(link)
+
+        return link_list
         
-    def get_data(self):
+    def get_data(self, link_list):
         """ 
         Method for extracting image and text data for each books. This method will load each book individually, extract the text data and download their respective images. 
 
@@ -99,51 +101,55 @@ class Scraper:
             id_list (list): List UUID4 reference for each of the books
             img_list (list): List SRC links for the main image of each book  
         """
-        self.price_list = []
-        self.name_list = []
-        self.isbn_list = []
-        self.id_list = []        
-        self.img_list = []
+        price_list = []
+        name_list = []
+        isbn_list = []
+        id_list = []        
+        img_list = []
 
-        for URL in self.link_list:
+        for URL in link_list:
             self.driver.get(URL)
             time.sleep(0.5)
             #Get text data
             price = self.driver.find_element(by=By.XPATH, value='//b[@itemprop="price"]').text
-            self.price_list.append(str(price))
+            price_list.append(str(price))
             name = self.driver.find_element(by=By.XPATH, value='//span[@class="book-title"]').text
-            self.name_list.append(name)
+            name_list.append(name)
             isbn = self.driver.find_element(by=By.XPATH, value='/html/body/div[1]/div[2]/div[2]/section[2]/div[2]/div/div/p/i[2]/span').text
-            self.isbn_list.append(isbn)
+            isbn_list.append(isbn)
             book_id = str(uuid.uuid4())
-            self.id_list.append(book_id)
+            id_list.append(book_id)
             #Get image data
             img_container =self.driver.find_element(by=By.XPATH, value='//div[@class="main-container"]') # XPath corresponding to the Container
             img_tag = img_container.find_element(by=By.XPATH, value='.//img[@itemprop="image"]')
             src = img_tag.get_attribute('src')
-            self.img_list.append(src)
+            img_list.append(src)
+        return price_list, name_list, isbn_list, id_list, img_list
 
-    def save_data_files(self):
+    def save_data_files(self, link_list, price_list, name_list, isbn_list, id_list, img_list):
 
         """Method for saving the data and images gathered to their respective folders and json files"""
         
         book_dict_list = []
 
-        for index in range(len(book_info.link_list)):
-            book_dict_list.append({'ID': book_info.id_list[index], 'ISBN': book_info.isbn_list[index], 'Price': book_info.price_list[index], 'Name': book_info.name_list[index]})
-        try:
-            os.mkdir(os.path.join('raw_data', str(book_dict_list[index]['ISBN'])))
+        for index in range(len(link_list)):
+            book_dict_list.append({'ID': id_list[index], 'ISBN': isbn_list[index], 'Price': price_list[index], 'Name': name_list[index]})
 
-        except FileExistsError:
-            print("Directory already exists")
+            try:
+                path = ['raw_data/', str(book_dict_list[index]['ISBN'])]
+                os.mkdir(''.join(path))
+                
+            except FileExistsError:
+                print("Directory already exists")
 
-        with open(os.path.join('raw_data', str(book_dict_list[index]['ISBN']),'data.json'), 'w') as fp:
-            json.dump(book_dict_list[index], fp)
 
-        path = ['raw_data/', str(book_dict_list[index]['ISBN']), '/',  str(book_dict_list[index]['ISBN']), '.jpg' ]
-        image = requests.get(str(book_info.img_list[index])).content
-        with open(''.join(path), "wb") as outimage:
-            outimage.write(image)
+            with open(os.path.join('raw_data', str(book_dict_list[index]['ISBN']),'data.json'), 'w') as fp:
+                json.dump(book_dict_list[index], fp)
+
+            path = ['raw_data/', str(book_dict_list[index]['ISBN']), '/',  str(book_dict_list[index]['ISBN']), '.jpg' ]
+            image = requests.get(str(img_list[index])).content
+            with open(''.join(path), "wb") as outimage:
+                outimage.write(image)
 
 
 if __name__ == "__main__":
@@ -151,9 +157,9 @@ if __name__ == "__main__":
     book_info.accept_cookies()
     #book_info.scroll(rep=3)
     #book_info.infinite_scroll()
-    book_info.get_links()
-    book_info.get_data()
-    book_info.save_data_files()
+    link_list = book_info.get_links()
+    price_list, name_list, isbn_list, id_list, img_list = book_info.get_data(link_list)
+    book_info.save_data_files(link_list, price_list, name_list, isbn_list, id_list, img_list)
 
 
 
